939cb49f699a2ed002e78a31dc1094ed19ad9dce
Colin Bates, Nick Rogers and Swati Sahay <nick@>
2018-08-15 15:27:08 -0700

    add edit method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,16 @@ export class AppComponent {
   kegs: Keg[] = [
     new Keg('Spicy Orange', 'Bud Night', 11, 2, 124)
   ];
-  selectedTask = null;
+
   submit(){
     this.kegs.push(new Keg(this.kegName, this.kegBrand, this.kegPrice, this.kegContent, this.kegPints))
   }
-  editTask(clickedTask) {
-      this.selectedTask = clickedTask;
-    }
-    finishedEditing() {
-    this.selectedTask = null;
+    selectedKeg= null;
+  editTask(currentKeg) {
+    this.selectedKeg= currentKeg;
   }
+  finishedEditing() {
+    console.log(this.kegs);
+  this.selectedKeg= null;
+}
 }
