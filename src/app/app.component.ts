037de7f134a64b4f3c31dae8d26fafc2e9afc964
Colin Bates, Nick Rogers and Swati Sahay <colin@>
2018-08-15 16:35:58 -0700

    add sold method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,26 @@ import { Keg } from './models/keg.model';
 export class AppComponent {
   title = 'app';
   kegs: Keg[] = [
-    new Keg('Spicy Orange', 'Bud Night', 11, 2, 124)
+    new Keg('Spicy Orange Dream', 'Bud Night', 11, 2, 98)
+    new Keg('Caramel Banana Surprise', 'Willer Lite', 5, 2, 23)
+    new Keg('Monster Grapefruit', 'Tres Equis', 33, 2, 111)
+    new Keg('Graveyard Sunshine', 'Doors Lite', 4, 2, 24)
+    new Keg('Organic Seaweed', 'Green Moon', 3, 2, 46)
+    new Keg('Crazy Frog', 'Sudweiser', 1, 2, 50)
   ];
+  kegName = null;
+  kegBrand = null;
+  kegPrice = null;
+  kegContent = null;
+  kegPints = null;
 
   submit(){
     this.kegs.push(new Keg(this.kegName, this.kegBrand, this.kegPrice, this.kegContent, this.kegPints))
+  }
+  sold(currentKeg){
+    if(currentKeg.pintsLeft > 0){
+      currentKeg.pintsLeft -= 1;
+    }
   }
     selectedKeg= null;
   editTask(currentKeg) {
