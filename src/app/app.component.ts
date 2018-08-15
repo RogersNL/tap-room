7b043e0bb0eafb2709e5c24279fe268efdc675a1
Colin Bates, Nick Rogers and Swati Sahay <colin@>
2018-08-15 14:39:51 -0700

    Submit new keg with form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  kegs: Keg[] = [
+    new Keg('Spicy Orange', 'Bud Night', 11, 2, 124)
+  ];
+  selectedTask = null;
+  submit(){
+    this.kegs.push(new Keg(this.kegName, this.kegBrand, this.kegPrice, this.kegContent, this.kegPints))
+  }
+  editTask(clickedTask) {
+      this.selectedTask = clickedTask;
+    }
+    finishedEditing() {
+    this.selectedTask = null;
+  }
 }
